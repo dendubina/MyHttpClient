be54f09395cc04c0f22df75e72e083e9a2c6a0cd
RapidRapidRabbit <awestruck31@mail.ru>
2022-07-17 18:35:17 +0300

    refactoring

--- AppForUsingClient/Program.cs
+++ AppForUsingClient/Program.cs
@@ -1,23 +1,12 @@
 ﻿using System;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
-using MyHttpClientProject;
-using MyHttpClientProject.Extensions;
 
 namespace AppForUsingClient
 {
     public class Program
     {
         static async Task Main(string[] args)
-        {
-            var client = new MyHttpClient();
-
-            var response = await client.GetAsync("http://rasfokus.ru/images/photos/medium/5490c3cb296af723d10bf3812079e145.jpg");
-
-            Console.WriteLine(response.StatusCode);
-
-            client.Dispose();
+        {            
             Console.WriteLine("Hello world!");
             Console.ReadLine();
         }
--- TestProject/Extensions/MyHttpClientExtensionTests.cs
+++ TestProject/Extensions/MyHttpClientExtensionTests.cs
@@ -15,89 +15,87 @@ namespace TestProject.Extensions
 {
     public class MyHttpClientExtensionTests
     {
+        private const HttpStatusCode ExpectedStatusCode = HttpStatusCode.OK;
+        private const string ExampleUri = "http://google.com";
+        private const string ExpectedResponseContent = "example content";
+
+        private static readonly IHttpBody _exampleRequestContent = new StringBody("request content");
+
+        private static readonly IEnumerable<byte> _expectedResponseContentBytes =
+            Encoding.UTF8.GetBytes(ExpectedResponseContent);
+
+        private static readonly IDictionary<string, string> _expectedHeaders = new Dictionary<string, string>
+        {
+            { "Connection", "keep-alive" },
+            { "Server", "gws" },
+            { "Content-Length", _expectedResponseContentBytes.Count().ToString() }
+        };
+
+        private static readonly string _responseHeadersString =
+            $"HTTP/1.1 {(int)ExpectedStatusCode} OK{Environment.NewLine}" +
+            $"Connection: keep-alive{Environment.NewLine}" +
+            $"Server: gws{Environment.NewLine}" +
+            $"Content-Length: {_expectedResponseContentBytes.Count()}{Environment.NewLine}";
+
         private readonly IMyHttpClient _httpClient;
-        private readonly Mock<IConnection> _mockedConnection;
-        private readonly IDictionary<string, string> _expectedHeaders;
-        private readonly HttpStatusCode _expectedStatusCode;
-        private readonly IEnumerable<byte> _expectedContent;
 
         public MyHttpClientExtensionTests()
         {
-            _expectedStatusCode = HttpStatusCode.OK;
+            var mockedConnection = new Mock<IConnection>();
 
-            var content = "example content";
-            int contentLength = Encoding.UTF8.GetByteCount(content);
-
-            _expectedContent = Encoding.UTF8.GetBytes(content);
-
-            _expectedHeaders = new Dictionary<string, string>
-            {
-                { "Connection", "keep-alive" },
-                { "Server", "gws" },
-                { "Content-Length", contentLength.ToString() }
-            };
-
-            var responseHeadersString =
-                $"HTTP/1.1 {(int)_expectedStatusCode} OK{Environment.NewLine}" +
-                $"Connection: keep-alive{Environment.NewLine}" +
-                $"Server: gws{Environment.NewLine}" +
-                $"Content-Length: {contentLength}{Environment.NewLine}";
-
-            _mockedConnection = new Mock<IConnection>();
-
-            _mockedConnection
+            mockedConnection
                 .Setup(x => x.ReadHeaders())
-                .Returns(Encoding.UTF8.GetBytes(responseHeadersString));
+                .Returns(Encoding.UTF8.GetBytes(_responseHeadersString));
 
-            _mockedConnection
+            mockedConnection
                 .Setup(x => x.ReadBodyAsync(It.IsAny<int>()))
-                .ReturnsAsync(_expectedContent);
+                .ReturnsAsync(Encoding.UTF8.GetBytes(ExpectedResponseContent));
 
-            _httpClient = new MyHttpClientProject.MyHttpClient(_mockedConnection.Object);
+            _httpClient = new MyHttpClientProject.MyHttpClient(mockedConnection.Object);
         }
 
         [Fact]
         public async void GetAsync_Returns_Expected_HttpResponse()
         {
             //Act
-            var actual = await _httpClient.GetAsync("http://google.com");
+            var actual = await _httpClient.GetAsync(ExampleUri);
 
             //Assert
-            Assert.Equal(_expectedStatusCode, actual.StatusCode);
+            Assert.Equal(ExpectedStatusCode, actual.StatusCode);
             Assert.Equal(_expectedHeaders, actual.ResponseHeaders);
-            Assert.Equal(_expectedContent, actual.ResponseBody.ToArray());
+            Assert.Equal(Encoding.UTF8.GetBytes(ExpectedResponseContent), actual.ResponseBody.ToArray());
         }
 
         [Fact]
         public async void PostAsync_Returns_Expected_HttpResponse()
         {
             //Act
-            var actual = await _httpClient.PostAsync("http://google.com", new StringBody("request content"));
+            var actual = await _httpClient.PostAsync(ExampleUri, _exampleRequestContent);
 
             //Assert
-            Assert.Equal(_expectedStatusCode, actual.StatusCode);
+            Assert.Equal(ExpectedStatusCode, actual.StatusCode);
             Assert.Equal(_expectedHeaders, actual.ResponseHeaders);
-            Assert.Equal(_expectedContent, actual.ResponseBody.ToArray());
+            Assert.Equal(Encoding.UTF8.GetBytes(ExpectedResponseContent), actual.ResponseBody.ToArray());
         }
 
         [Fact]
         public async void PostWithStringResponseAsync_Returns_Expected_String()
         {
             //Act
-            var actual = await _httpClient.PostWithStringResponseAsync("http://google.com", new StringBody("request content"));
+            var actual = await _httpClient.PostWithStringResponseAsync(ExampleUri, _exampleRequestContent);
 
             //Assert
-            Assert.Equal(Encoding.UTF8.GetString(_expectedContent.ToArray()), actual);
+            Assert.Equal(Encoding.UTF8.GetString(_expectedResponseContentBytes.ToArray()), actual);
         }
 
         [Fact]
         public async void PostWithByteArrayResponseAsync_Returns_Expected_ByteArray()
         {
             //Act
-            var actual = await _httpClient.PostWithByteArrayResponseAsync("http://google.com", new StringBody("request content"));
+            var actual = await _httpClient.PostWithByteArrayResponseAsync(ExampleUri, _exampleRequestContent);
 
             //Assert
-            Assert.Equal(_expectedContent.ToArray(), actual);
+            Assert.Equal(_expectedResponseContentBytes.ToArray(), actual);
         }
 
         [Fact]
@@ -107,11 +105,11 @@ namespace TestProject.Extensions
             using var memoryStream = new MemoryStream();
 
             //Act
-            var actual = await _httpClient.PostWithStreamResponseAsync("http://google.com", new StringBody("request content"));
+            var actual = await _httpClient.PostWithStreamResponseAsync(ExampleUri, _exampleRequestContent);
             await actual.CopyToAsync(memoryStream);
 
             //Assert
-            Assert.Equal(_expectedContent.ToArray(), memoryStream.ToArray());
+            Assert.Equal(_expectedResponseContentBytes.ToArray(), memoryStream.ToArray());
         }
     }
 }
