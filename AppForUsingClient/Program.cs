032d19c94b21a963b9b3d39e5d996f5052d681a4
RapidRapidRabbit <awestruck31@mail.ru>
2022-08-15 01:57:16 +0300

    Update Program.cs

--- AppForUsingClient/Program.cs
+++ AppForUsingClient/Program.cs
@@ -7,24 +7,7 @@ namespace AppForUsingClient
     public class Program
     {
         static async Task Main(string[] args)
-        {
-            var line = "Connection: close    g";
-
-            var pattern = @"(\w+):\s(\w+)";
-
-            
-            var result = new Regex(pattern).Match(line);
-
-            Console.WriteLine(result.Success);
-
-            foreach (var group in result.Groups)
-            {
-                Console.WriteLine(group);
-            }
-
-
-
-
+        {            
             Console.WriteLine("Hello world!");
             Console.ReadLine();
         }
